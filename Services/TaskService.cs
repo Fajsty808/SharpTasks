c30e11a16e992482a0b9344f3c73a0f744b80b9a
Martin <martaszemanek@seznam.cz>
2025-04-05 14:28:03 +0200

    Adding methods for tasks in TaskService class

--- Services/TaskService.cs
+++ Services/TaskService.cs
@@ -8,8 +8,36 @@ public class TaskService
 
     // Next, we’ll implement:
     // - GetAll()
+    public IEnumerable<TaskItem> GetAll()
+    {
+        return _tasks;
+    }   
     // - GetById(Guid id)
+    public TaskItem? GetById(Guid id)
+    {
+        return _tasks.FirstOrDefault(task => task.Id == id);
+    }
     // - Add(TaskItem task)
+    public void AddTask(TaskItem task)
+    {
+        _tasks.Add(task);
+    }
     // - Update(TaskItem task)
+    public bool UpdateTask(TaskItem task)
+    {
+        var index = _tasks.FindIndex(t => t.Id == task.Id);
+        if (index == -1) return false;
+
+        _tasks[index] = task;
+        return true;
+    }
     // - Delete(Guid id)
+    public bool DeleteTask(Guid id)
+    {
+        var index = _tasks.FindIndex(t => t.Id == id);
+        if (index == -1) return false;
+
+        _tasks.RemoveAt(index);
+        return true;
+    }
 }
